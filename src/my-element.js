5a5f3febd108f2dc73aafae6ea5bbefe71b757e1
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-05-20 02:22:07 -0500

    feat: :building_construction: Estructure updated
    Chart needs to be finished

--- src/my-element.js
+++ src/my-element.js
@@ -1,47 +1,33 @@
-import { LitElement, css, html } from 'lit'
+import { LitElement, css, html } from 'lit';
 
 export class MyElement extends LitElement {
   static styles = css`
-    
-
-    :host{
+    :host {
       --clr-main: #4b33a8;
       --clr-main-light: #785ce9;
       --clr-white: #ececec;
       --clr-gray: #e2e2e2;
       --clr-red: #961818;
-    }
-    *{
       margin: 0;
       padding: 0;
       box-sizing: border-box;
       font-family: 'Rubik', sans-serif;
     }
 
-    h1, h2, h3, h4, h5, h6, p, a, input, textarea, ul{
-      margin: 0;
-      padding: 0;
-    }
-
     ul {
       list-style-type: none;
-    }
-
-    a{
-      text-decoration: none;
-      color: var(--clr-main);
     }
 
     .wrapper {
       display: grid;
       grid-template-columns: 1fr 4fr;
       background-color: var(--clr-main);
-
     }
-    aside{
+
+    aside {
       padding: 2rem;
       padding-right: 0;
-      color: white;
+      color: var(--clr-white);
       position: sticky;
       top: 0;
       height: 100vh;
@@ -50,18 +36,18 @@ export class MyElement extends LitElement {
       justify-content: space-between;
     }
 
-    .logo{
+    .logo {
       font-weight: 400;
       font-size: 1.3rem;
     }
 
-    .menu{
+    .menu {
       display: flex;
       flex-direction: column;
       gap: .5rem;
     }
 
-    .boton-menu{
+    .boton-menu {
       background-color: transparent;
       border: 0;
       color: var(--clr-white);
@@ -75,7 +61,7 @@ export class MyElement extends LitElement {
       width: 100%;
     }
 
-    .boton-menu.active{
+    .boton-menu.active {
       background-color: var(--clr-white);
       color: var(--clr-main);
       border-top-left-radius: 1rem;
@@ -83,17 +69,19 @@ export class MyElement extends LitElement {
       position: relative;
     }
 
-    .boton-menu.active::before{
+    .boton-menu.active::before {
       content: '';
       position: absolute;
       width: 1rem;
       height: 2rem;
+      bottom: 100%;
+      right: 0;
       background-color: transparent;
       border-bottom-right-radius: .5rem;
       box-shadow: 0 1rem 0 var(--clr-white);
     }
 
-    .boton-menu.active::after{
+    .boton-menu.active::after {
       content: '';
       position: absolute;
       width: 1rem;
@@ -105,23 +93,23 @@ export class MyElement extends LitElement {
       box-shadow: 0 -1rem 0 var(--clr-white);
     }
 
-    .boton-carrito{
+    .boton-carrito {
       margin-top: 2rem;
     }
 
-    .numerito{
+    .numerito {
       background-color: var(--clr-white);
       color: var(--clr-main);
       padding: .15rem .25rem;
       border-radius: .25rem;
     }
 
-    .boton-carrito.active .numerito{
+    .boton-carrito.active .numerito {
       background-color: var(--clr-main);
       color: var(--clr-white);
     }
 
-    .texto-footer{
+    .texto-footer {
       color: var(--clr-main-light);
       font-size: .85rem;
     }
@@ -134,20 +122,16 @@ export class MyElement extends LitElement {
       padding: 3rem;
     }
 
-    .titulo-principal{
+    .titulo-principal {
       color: var(--clr-main);
       margin-bottom: 2rem;
-
     }
 
     .contenedor-productos {
       display: grid;
       grid-template-columns: repeat(4, 1fr);
-      grid-gap: 1rem;
+      gap: 1rem;
     }
-
-
-    
 
     .product {
       border: 1px solid #ccc;
@@ -158,8 +142,47 @@ export class MyElement extends LitElement {
       flex-direction: column;
       align-items: center;
     }
+
     .product img {
       max-width: 100px;
+    }
+
+    .producto-imagen {
+      max-width: 100%;
+      border-radius: 1rem;
+    }
+
+    .producto-detalles {
+      background-color: var(--clr-main);
+      color: var(--clr-white);
+      padding: .5rem;
+      border-radius: 1rem;
+      margin-top: -2rem;
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      gap: .25rem;
+    }
+
+    .producto-titulo {
+      font-size: 1rem;
+    }
+
+    .producto-agregar {
+      border: 0;
+      background-color: var(--clr-white);
+      color: var(--clr-main);
+      padding: .4rem;
+      text-transform: uppercase;
+      border-radius: 2rem;
+      cursor: pointer;
+      border: 2px solid var(--clr-white);
+      transition: background-color .2s, color .2s;
+    }
+
+    .producto-agregar:hover {
+      background-color: var(--clr-main);
+      color: var(--clr-white);
     }
   `;
 
@@ -167,6 +190,8 @@ export class MyElement extends LitElement {
     abrigos: { type: Array },
     camisetas: { type: Array },
     pantalones: { type: Array },
+    categoriaSeleccionada: { type: String },
+
   };
 
   constructor() {
@@ -174,6 +199,8 @@ export class MyElement extends LitElement {
     this.abrigos = [];
     this.camisetas = [];
     this.pantalones = [];
+    this.categoriaSeleccionada = 'todos';
+
   }
 
   connectedCallback() {
@@ -181,111 +208,118 @@ export class MyElement extends LitElement {
     this.fetchData();
   }
 
-  async fetchData(category) {
-    const url = "https://file.notion.so/f/f/eaa1771c-fc19-40d4-8527-37ca1caab8fa/8f181ea0-47f7-49a5-9b85-48db35d8ec38/Documentos_DB.json?id=a21b973c-4a2b-4e71-b3f3-1b6e38a01f05&table=block&spaceId=eaa1771c-fc19-40d4-8527-37ca1caab8fa&expirationTimestamp=1715983200000&signature=bKCU1aN7ehQWRZuMrzXneZB3f7zYUNNl8sNhZD3IwDk&downloadName=Documentos_DB.json"; // Reemplaza esta URL con la URL real de tu JSON
+  async fetchData() {
     try {
-      const response = await fetch(url);
+      // const abrigosResponse = await fetch('http://localhost:5501/abrigo');
+      // const camisetasResponse = await fetch('http://localhost:5501/camiseta');
+      // const pantalonesResponse = await fetch('http://localhost:5501/pantalon');
+      // this.abrigos = await abrigosResponse.json();
+      // this.camisetas = await camisetasResponse.json();
+      // this.pantalones = await pantalonesResponse.json();
+      const response = await fetch('http://localhost:5501/articles');
       const data = await response.json();
-      if(data.abrigo && Array.isArray(data.abrigo)) this.abrigos = data.abrigo;
-      if(data.camiseta && Array.isArray(data.camiseta)) this.camisetas = data.camiseta;
-      if(data.pantalon && Array.isArray(data.pantalon)) this.pantalones = data.pantalon;
-
+      this.abrigos = await data.abrigo;
+      this.camisetas = data.camiseta;
+      this.pantalones = data.pantalon;
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   }
 
   render() {
-    return html`
 
-    <div class="wrapper">
-    <header class="header-mobile">
-        <h1 class="logo">CarpiShop</h1>
-        <button class="open-menu" id="open-menu">
-            <i class="bi bi-list"></i>
-        </button>
-    </header>
-    <aside>
-        <button class="close-menu" id="close-menu">
-            <i class="bi bi-x"></i>
-        </button>
-        <header>
-            <h1 class="logo">CarpiShop</h1>
-        </header>
-        <nav>
+    let productos = [];
+    switch (this.categoriaSeleccionada) {
+      case 'abrigos':
+        productos = this.abrigos;
+        break;
+      case 'camisetas':
+        productos = this.camisetas;
+        break;
+      case 'pantalones':
+        productos = this.pantalones;
+        break;
+      default:
+        productos = [...this.abrigos, ...this.camisetas, ...this.pantalones];
+    }
+    return html`
+      <div class="wrapper">
+        <aside>
+          <header>
+            <h1 class="logo">CampusLands</h1>
+          </header>
+          <nav>
             <ul class="menu">
-                <li>
-                    <button id="todos" class="boton-menu boton-categoria active"><i class='bx bxs-hand-right'></i>Todos los productos</button>
-                </li>
-                <li>
-                    <button id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Abrigos</button>
-                </li>
-                <li>
-                    <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Camisetas</button>
-                </li>
-                <li>
-                    <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Pantalones</button>
-                </li>
-                <li>
-                    <a class="boton-menu boton-carrito" href="#">
-                        <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">0</span>
-                    </a>
-                </li>
+              <li>
+                <button id="todos" class="boton-menu boton-categoria active"><i class='bx bxs-hand-right'></i>Todos los productos</button>
+              </li>
+              <li>
+                <button @click=${() => this.cambiarCategoria('abrigos')} id="abrigos" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Abrigos</button>
+              </li>
+              <li>
+                <button id="camisetas" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Camisetas</button>
+              </li>
+              <li>
+                <button id="pantalones" class="boton-menu boton-categoria"><i class="bi bi-hand-index-thumb"></i> Pantalones</button>
+              </li>
+              <li>
+                <a class="boton-menu boton-carrito" href="#">
+                  <i class="bi bi-cart-fill"></i> Carrito <span id="numerito" class="numerito">0</span>
+                </a>
+              </li>
             </ul>
-        </nav>
-        <footer>
+          </nav>
+          <footer>
             <p class="texto-footer">© 2022 Carpi Coder</p>
-        </footer>
-    </aside>
-    <main>
+          </footer>
+        </aside>
+      <main>
         <h2 class="titulo-principal" id="titulo-principal">Todos los productos</h2>
-        <div id="contenedor-productos" class="contenedor-productos">
-            <!-- Esto se va a rellenar con JS -->
-            
+        <div class="contenedor-productos">
+          ${this.abrigos.map(
+            abrigo => html`
+              <div class="product">
+                <img class="producto-imagen" src="${abrigo.imagen}" alt="${abrigo.nombre}" />
+                <div class="producto-detalles">
+                  <p class="producto-titulo">${abrigo.nombre}</p>
+                  <p>${abrigo.precio} COP</p>
+                  <button class="producto-agregar">Agregar al carrito</button>
+                </div>
+              </div>
+            `
+          )}
+          ${this.camisetas.map(
+            camiseta => html`
+              <div class="product">
+                <img class="producto-imagen" src="${camiseta.imagen}" alt="${camiseta.nombre}" />
+                <div class="producto-detalles">
+                  <p class="producto-titulo">${camiseta.nombre}</p>
+                  <p>${camiseta.precio} COP</p>
+                  <button class="producto-agregar">Agregar al carrito</button>
+                </div>
+              </div>
+            `
+          )}
+          ${this.pantalones.map(
+            pantalon => html`
+              <div class="product">
+                <img class="producto-imagen" src="${pantalon.imagen}" alt="${pantalon.nombre}" />
+                <div class="producto-detalles">
+                  <p class="producto-titulo">${pantalon.nombre}</p>
+                  <p>${pantalon.precio} COP</p>
+                  <button class="producto-agregar">Agregar al carrito</button>
+                </div>
+              </div>
+            `
+          )}
         </div>
-    </main>
-</div>
-<h1>Productos</h1>
-      <h2>Abrigos</h2>
-      <div>
-        ${Array.isArray(this.abrigos) ? this.abrigos.map(
-          abrigo => html`
-            <div class="product">
-              <img src="${abrigo.imagen}" alt="${abrigo.nombre}" />
-              <p>${abrigo.nombre}</p>
-              <p>${abrigo.precio} COP</p>
-            </div>
-          `
-        ) : 'No se encontraron abrigos'}
+      </main>
       </div>
-      <h2>Camisetas</h2>
-      <div>
-        ${Array.isArray(this.camisetas) ? this.camisetas.map(
-          camiseta => html`
-            <div class="product">
-              <img src="${camiseta.imagen}" alt="${camiseta.nombre}" />
-              <p>${camiseta.nombre}</p>
-              <p>${camiseta.precio} COP</p>
-            </div>
-          `
-        ) : 'No se encontraron camisetas'}
-      </div>
-      <h2>Pantalones</h2>
-      <div>
-        ${Array.isArray(this.pantalones) ? this.pantalones.map(
-          pantalon => html`
-            <div class="product">
-              <img src="${pantalon.imagen}" alt="${pantalon.nombre}" />
-              <p>${pantalon.nombre}</p>
-              <p>${pantalon.precio} COP</p>
-            </div>
-          `
-        ) : 'No se encontraron pantalones'}
-      </div>
-      
     `;
+    }
+  cambiarCategoria(categoria) {
+    this.categoriaSeleccionada = categoria;
   }
 }
 
-
-customElements.define('my-element', MyElement)
+customElements.define('my-element', MyElement);
